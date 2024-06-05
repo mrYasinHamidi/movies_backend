ac7e749e0a6881d963e74383560d4d216e945e2b
yas <yas@gmail.com>
2024-06-05 20:48:49 +0330

    change routes order

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const port = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/api', (req, res) => {
+    res.send('API is working');
+});
+
 // Define a route to serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/api', (req, res) => {
-    res.send('API is working');
 });
 
 // Start the server
