0585ab229870376b2fe22885f8e1cb1dad863ddc
yas <yas@gmail.com>
2024-06-05 20:22:03 +0330

    change routes

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.send('API is working');
 });
-app.use('/api/auth', router);
+app.use(router);
 
 app.listen(5000, () => console.log(`Server started`));
