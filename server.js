0c64c4d3814b7ee6ada9cebe33db63b8ccfc52aa
yas <yas@gmail.com>
2024-06-05 20:34:28 +0330

    show an html page

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-// server.js
+// // server.js
+// const express = require('express');
+//
+//
+// const app = express();
+// const router = express.Router();
+//
+// router.get('/', (req, res) => {
+//     res.send('API is working');
+// });
+// app.use(router);
+//
+// app.listen(5000, () => console.log(`Server started`));
 const express = require('express');
-
+const path = require('path');
 
 const app = express();
-const router = express.Router();
+const port = 3000;
 
-router.get('/', (req, res) => {
-    res.send('API is working');
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.use(router);
 
-app.listen(5000, () => console.log(`Server started`));
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
