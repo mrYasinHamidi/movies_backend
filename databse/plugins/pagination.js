b730f603e866c748faf80b9c0c3e61744357b8c7
yas <yas@gmail.com>
2024-09-03 21:11:10 +0330

    change pagination keys

--- databse/plugins/pagination.js
+++ databse/plugins/pagination.js
@@ -2,13 +2,13 @@ module.exports = function paginationPlugin(schema) {
     schema.statics.paginate = async function (query, page = 1, limit = 10) {
         const skip = (parseInt(page, 10) - 1) * parseInt(limit, 10);
         const total = await this.countDocuments(query);
-        const results = await this.find(query).skip(skip).limit(parseInt(limit, 10));
+        const data = await this.find(query).skip(skip).limit(parseInt(limit, 10));
 
         return {
-            results,
+            data,
             total,
-            page: parseInt(page, 10),
-            pages: Math.ceil(total / parseInt(limit, 10)),
+            currentPage: parseInt(page, 10),
+            totalPage: Math.ceil(total / parseInt(limit, 10)),
         };
     };
 };
